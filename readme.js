9c0915da759a2dc5340f8cad84d995b6a16be9cf
MDZZ-LBJ <13041895753@163.com>
2021-05-14 11:38:03 +0800

    ''

---
+++ readme.js
@@ -0,0 +1,21 @@
+/* 
+
+
+  新建项目：create-react-app 项目名
+
+  全局安装：npm install create-react-app -g
+
+  不全局安装; npx create-react-app 项目名
+
+*/
+
+/* 
+
+  vue scoped局部样式 原理：给元素加行内属性 data-v-2b58cc6... + 属性选择器
+
+  react 局部样式： xxx.moudle.css 
+   import style from "./xxx.module.css";
+   console.log(style); App: "xxx_App__3vKj6"
+   <div className={"App " + style.App}>Learn React</div>
+  
+*/
