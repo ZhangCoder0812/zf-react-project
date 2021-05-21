7960131a0a46b9820169f6e52ce047d058ad7573
MDZZ-LBJ <13041895753@163.com>
2021-05-22 00:14:34 +0800

    -

--- src/24 - 高阶组件3.js
+++ src/24 - 高阶组件3.js
@@ -1,30 +1,29 @@
 import React from "react";
 
 function connect(f1, f2) {
-    let propsObj = f1();
-    let fnObj = f2();
-    return function(Com) {
-        return function() {
-            return <Com {...propsObj} {...fnObj} />;
-        };
+  let propsObj = f1({ name: "wade" });
+  let fnObj = f2({ dispatch: () => {} });
+  return function (Com) {
+    return function () {
+      return <Com {...propsObj} {...fnObj} />;
     };
+  };
 }
 
 class App extends React.Component {
-    render() {
-        console.log(this.props);
-        return <div>App</div>;
-    }
+  render() {
+    console.log(this.props);
+    return <div>App</div>;
+  }
 }
+// 可以通过这种方式给App传递参数
 App = connect(
-    () => ({
-        name: "wade",
-    }),
-    () => ({
-        fn: () => {
-            console.log("fn");
-        },
-    })
+  (state) => ({
+    name: state.name,
+  }),
+  (dispatch) => ({
+    dispatch,
+  })
 )(App);
 
 export default App;
