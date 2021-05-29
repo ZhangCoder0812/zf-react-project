cdf2ea491cf09c9cd700f66c8897bf30b689328d
MDZZ-LBJ <13041895753@163.com>
2021-05-30 00:42:58 +0800

    -

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,9 +1,21 @@
 import React from "react";
+import { Route } from "react-router-dom";
 
 class About extends React.Component {
   render() {
     console.log(this.props.location.state); // 刷新发现路由里的 sate不会丢失
-    return <div>About</div>;
+    return (
+      <div>
+        About
+        {/* 这里也可以写 Route 实现嵌套路由 */}
+        <Route
+          path="/about/xxx"
+          render={() => {
+            return <h1>我是about子路由</h1>;
+          }}
+        ></Route>
+      </div>
+    );
   }
 }
 
